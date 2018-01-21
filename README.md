2842183ddf22e5a3112b9224a46b9c6561c23054
Alex <zed-chi@mail.ru>
2018-01-21 22:09:01 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Zedchi\par-parser
+Проверяет правильность  расстановки скобок
+
+#версия
+0.1
+
+#установка
+composer require zed-chi/par-parser
+
+#использование
+$parser = new Zedchi\Parser();
+$parser->parse("строка");
---
+++ src/verify.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Zedchi\Verify;
+
+class Verify
+{
+    public function parse($str)
+    {
+        /**/
+        $re = '/[^\(,\),\n,\t,\r," "]/';
+        if (strlen($str) == 0){
+            return false;
+        }
+        if (preg_match($re, $str)) {
+            throw new \Exception("Unknown Symbol");
+        }
+        /**/
+        
+        $x = 0;
+        for ($i = 0; $i < strlen($str); $i++) 
+        {
+            if ( $str[$i] == "(" ) {$x++;} else if ($str[$i] == ")") {$x--;}
+            if ( $x < 0 ) {return false;}
+        }
+        if ($x == 0){return true;}
+        else{return false;}
+    }
+}
